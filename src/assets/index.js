14e43183c1fd675648eece9c525460e823b32178
Ledanh3010 <nguyenledanh19@gmail.com>
2024-09-06 18:41:43 +0900

    delete some file

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,5 @@
 import logo from "./logo.svg";
-import backend from "./backend.png";
-import creator from "./creator.png";
-import mobile from "./mobile.png";
-import web from "./web.png";
+
 import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
@@ -30,17 +27,10 @@ import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 import threejs from "./tech/threejs.svg";
 
-import meta from "./company/meta.png";
-import shopify from "./company/shopify.png";
-import starbucks from "./company/starbucks.png";
-import tesla from "./company/tesla.png";
 import btech from "./company/btech.png";
 import tokuden from "./company/tokuden.png";
 import planex from "./company/planex.png";
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
 import seihin from "./seihin.png";
 import meisterWork from "./meisterWork.png";
 import catiaWork from "./catiaWork.png";
@@ -54,10 +44,6 @@ import projectItMusic from "./projectItMusic.png";
 
 export {
   logo,
-  backend,
-  creator,
-  mobile,
-  web,
   github,
   menu,
   close,
@@ -74,13 +60,6 @@ export {
   tailwind,
   typescript,
   threejs,
-  meta,
-  shopify,
-  starbucks,
-  tesla,
-  carrent,
-  jobit,
-  tripguide,
   btech,
   tokuden,
   planex,
