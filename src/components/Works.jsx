553d5727ac96415dd22ead62a00e57044abddac1
Ledanh3010 <nguyenledanh19@gmail.com>
2024-09-18 22:44:20 +0900

    fix text

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
